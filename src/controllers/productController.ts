58d478799aaab7e85644d17ddfa2e7f843865669
Eyimofe <eyimofe36@gmail.com>
2025-12-02 18:43:59 +0100

    Added controllers

---
+++ src/controllers/productController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+export function listProducts(req: Request, res: Response) {
+  res.json({ message: "ProductList Endpoint" });
+}
+
+export function getProductById(req: Request, res: Response) {
+  const productId = req.params["id"];
+  res.json({ message: `ProductView Endpoint: ${productId}` });
+}
+
+export function createProduct(req: Request, res: Response) {
+  res.json({ message: "Create Product Endpoint" });
+}
+
+export function updateProduct(req: Request, res: Response) {
+  const productId = req.params["id"];
+  res.json({ message: `ProductUpdate Endpoint: ${productId}` });
+}
+
+export function deleteProduct(req: Request, res: Response) {
+  const productId = req.params["id"];
+  res.json({ message: `ProductDelete Endpoint: ${productId}` });
+}
